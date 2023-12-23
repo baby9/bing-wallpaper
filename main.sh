2b51ab93bef7fc83446aeb1c009ccb66b5af4300
baby9 <67196418+baby9@users.noreply.github.com>
2023-12-23 10:52:13 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -3,11 +3,11 @@
 base_url="https://www.bing.com"
 api="HPImageArchive.aspx?format=js&idx=0&n=1"
 header="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36"
-json=$(curl -s -A "$header" -X GET "${base_url}/${api}" -H "Accept: application/json")
-full_url="${base_url}$(echo $json | awk -F '"url"' '{print $2}' | awk -F '"' '{print $2}')"
+response=$(curl -s -A "$header" -X GET "${base_url}/${api}" -H "Accept: application/json")
+full_url="${base_url}$(echo $response | awk -F '"url"' '{print $2}' | awk -F '"' '{print $2}')"
 
 image_url=$(echo $full_url | awk -F '&' '{print $1}')
-copyright=$(echo $json | awk -F '"copyright"' '{print $2}' | awk -F '"' '{print $2}')
+copyright=$(echo $response | awk -F '"copyright"' '{print $2}' | awk -F '"' '{print $2}')
 image_UHD=$(echo $image_url | sed "s#$(echo $image_url | awk -F '.' '{print $(NF-1)}' | awk -F '_' '{print $NF}')#UHD#g")
  
 curl -s $image_UHD -o wallpaper.jpg
