35f458aea293cdcf2eb5258ff5e6c084e0be0e01
baby9 <67196418+baby9@users.noreply.github.com>
2024-07-30 05:56:14 +0000

    update comments

--- web/CloudflareWorkers.js
+++ web/CloudflareWorkers.js
@@ -3,17 +3,17 @@ addEventListener('fetch', event => {
 });
 
 async function handleRequest(request) {
-  // 设置请求头
+  // set header
   const headers = {
     'Accept-Language': 'en-US,en;q=0.9',
-    'User-Agent': `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36`,
+    'User-Agent': `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 Edg/120.0.2592.113`,
   };
 
-  // 获取图片 URL
+  // get image URL
   const response = await fetch('https://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=1&mkt=en-US', { headers });
   const data = await response.json();
   const imageUrl = `https://www.bing.com${data.images[0].url}`;
 
-  // 执行重定向操作
+  // redirect
   return Response.redirect(imageUrl, 301);
 }
