c1f30d03c7f74a7a813df643333d34fd55b5722b
baby9 <67196418+baby9@users.noreply.github.com>
2025-05-13 12:15:39 +0000

    Update README.md

--- main.sh
+++ main.sh
@@ -15,13 +15,21 @@ function updateREADME {
 
     if [ ! -z $urlbase ]; then
         if (! $(head README.md | grep -q "$urlbase")); then
-            new_date=$(date -d "$(echo $response | awk -F '"startdate"' '{print $2}' | awk -F '"' '{print $2}')" +%b-%d)
-            full_url="${base_url}$(echo $response | awk -F '"url"' '{print $2}' | awk -F '"' '{print $2}')"
-            image_url=$(echo $full_url | awk -F '&' '{print $1}')
+            startdate=$(echo $response | awk -F '"startdate"' '{print $2}' | awk -F '"' '{print $2}')
+            date_month_day=$(date -d $startdate +%b-%d)
+            date_year=$(date -d $startdate +%Y)
+            url="${base_url}$(echo $response | awk -F '"url"' '{print $2}' | awk -F '"' '{print $2}')"
+            image_url=$(echo $url | awk -F '&' '{print $1}')
             copyright=$(echo $response | awk -F '"copyright"' '{print $2}' | awk -F '"' '{print $2}')
             image_UHD=$(echo $image_url | sed "s#$(echo $image_url | awk -F '.' '{print $(NF-1)}' | awk -F '_' '{print $NF}')#UHD#g")
+            update_content="![](${image_url}&w=1000)${date_month_day}: [${copyright}](${image_UHD})<br><br>"
 
-            sed -i "1a ![](${image_url}&w=1000)${new_date}:\ [${copyright}](${image_UHD})<br><br>" README.md
+            sed -i "1a $update_content" README.md
+            sed -i '32,$d' README.md
+            if [ ! -d history/${date_year} ]; then
+              mkdir -p history/${date_year}
+            fi
+            echo $update_content >> history/${date_year}/README.md
         fi
     else
         exit 1
